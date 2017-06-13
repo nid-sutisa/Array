3227cd47adf042783ff8e795004e06bea702cd16
sutisa <a_sutisa@snru.ac.th>
2017-06-13 13:39:50 +0700

    addArray

--- Array.playground/Contents.swift
+++ Array.playground/Contents.swift
@@ -13,10 +13,18 @@ intArrayNumber2 = [55,66,77,88]
 
 //String Type
 var strArrayName1 = ["Doramon","Nobita","Cat"]
-var strArrayNumber2 = [String]()
-strArrayNumber2 = ["AAA","BBB","CCC"]
+var strArrayName2 = [String]()
+strArrayName2 = ["AAA","BBB","CCC"]
 
 //Double
 var douArrayNum3 = [1.2,1.3,1.4]
 var douArrayNum4 = [Double]()
 douArrayNum4 = [2.1,2.2,2.3]
+
+//add data to array
+print("intArrayNumber2 ==> \(intArrayNumber2)")
+intArrayNumber2.append(111)
+
+strArrayName2
+strArrayName2.append("DDD")
+
