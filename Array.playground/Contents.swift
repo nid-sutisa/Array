7afa4dbc4d344f24ad94e456143fe8cd91920632
sutisa <a_sutisa@snru.ac.th>
2017-06-13 13:24:34 +0700

    StartApp

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Array Variable
+//define variable and value
+//Implicit
+let intArrayNumber1 = [11,22,33,44]
+
+//Explicit
+var intArrayNumber2 = [Int]()
+intArrayNumber2 = [55,66,77,88]
+
+//String Type
+var strArrayName1 = ["Doramon","Nobita","Cat"]
+var strArrayNumber2 = [String]()
+strArrayNumber2 = ["AAA","BBB","CCC"]
+
+//Double
+var douArrayNum3 = [1.2,1.3,1.4]
+var douArrayNum4 = [Double]()
+douArrayNum4 = [2.1,2.2,2.3]
